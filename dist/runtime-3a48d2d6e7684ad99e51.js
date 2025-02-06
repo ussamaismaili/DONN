d14e75caeabdb8c4ad542d3cd502c0148c9b3306
ussamaismaili <ismailiossama@gmail.com>
2025-02-07 00:01:20 +0100

    New repo edit

--- dist/runtime-3a48d2d6e7684ad99e51.js
+++ dist/runtime-3a48d2d6e7684ad99e51.js
@@ -190,7 +190,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkwebpack_demo"] = self["webpackChunkwebpack_demo"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkDONN"] = self["webpackChunkDONN"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
